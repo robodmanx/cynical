655316c0dfd695605c428130df1ac6225dbf549f
Nunya <cynical_modder@hotmail.com>
2022-07-16 15:56:30 -0500

    Add files via upload

---
+++ loadstrings.lua
@@ -0,0 +1,21 @@
+if game.PlaceId == 6769764667 then
+    loadstring(game:HttpGet('https://scripts.luawl.com/Fusion/FactorySimulator.lua'))()
+elseif game.PlaceId == 9281034297 then 
+    loadstring(game:HttpGet('https://scripts.luawl.com/Fusion/GoalKickSimulator.lua'))()
+elseif game.PlaceId == 13822889 then
+    loadstring(game:HttpGet('https://scripts.luawl.com/Fusion/LT2.lua'))()
+elseif game.PlaceId == 10042772221 then
+    loadstring(game:HttpGet('https://scripts.luawl.com/Fusion/LT2Unsecure.lua'))()
+elseif game.PlaceId == 258258996 or game.PlaceId == 4780479031 then
+    loadstring(game:HttpGet('https://scripts.luawl.com/Fusion/MinersHaven.lua'))()
+elseif game.PlaceId == 6284583030 then
+    loadstring(game:HttpGet('https://scripts.luawl.com/Fusion/PetSimulator.lua'))()
+elseif game.PlaceId == 8781849572 then
+    loadstring(game:HttpGet('https://scripts.luawl.com/Fusion/StarSimulator.lua'))()
+elseif game.PlaceId == 8750997647 then
+    loadstring(game:HttpGet('https://scripts.luawl.com/Fusion/TappingLegendsX.lua'))()
+elseif game.PlaceId == 1962086868 or game.PlaceId == 3582763398 or game.PlaceId == 5253186791 then
+    loadstring(game:HttpGet('https://scripts.luawl.com/Fusion/MinersHaven.lua'))()
+elseif game.PlaceId == 171391948 then
+    loadstring(game:HttpGet('https://scripts.luawl.com/Fusion/VehicleSim.lua'))()
+end
